c45c987617846535a721051fe791cc279575e332
asiburrr <asiburaravi2005@gmail.com>
2025-02-22 00:12:44 +0600

    Update index.php

--- register/index.php
+++ register/index.php
@@ -465,8 +465,7 @@
     </div>
     </div>
     <?php include('../footer.php'); ?>
-    <!-- Bootstrap JS and Custom Script -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
     <script>
         const steps = document.querySelectorAll('.step');
         const stepItems = document.querySelectorAll('.step-item');
